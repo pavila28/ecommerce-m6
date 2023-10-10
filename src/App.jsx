53e2db0d3e56e7c405c70d0ba8f02455ce5bd1ef
Patricio Avila <pavila@vialterna.com>
2023-10-10 10:47:30 -0600

    AuthContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './Context/AuthContext'
 import Header from './Components/Header/Header'
 import RoutesIndex from './Routes/RoutesIndex'
 
 function App () {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <RoutesIndex />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          <RoutesIndex />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
